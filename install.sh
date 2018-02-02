e02cd0d20402da62a2388bce3fa75949ab4b8732
Paul <win10@list.ru>
2018-02-02 09:22:12 +0400

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#pacman -S i3 compton conky nitrogen rofi maim xclip playerctl
+pacman -S i3 compton conky nitrogen rofi maim xclip playerctl
 cp -R .i3 ~/.config/i4
-#cp .compton.conf ~/
-#cp .conkyrc ~/
+cp .compton.conf ~/
+cp .conkyrc ~/
 
 git clone https://github.com/unix121/i3wm-themer
-#cd i3wm-themer/scripts/
-#sh i3wmthemer -b i3.backup
-#sh i3wmthemer -c
-#sh i3wmthemer -t Forest
+cd i3wm-themer/scripts/
+sh i3wmthemer -b i3.backup
+sh i3wmthemer -c
+sh i3wmthemer -t Forest
 
