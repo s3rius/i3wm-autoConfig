7a1246a494932d117a0e479f99c58ef1b753288d
Paul <win10@list.ru>
2018-02-05 04:33:48 +0400

    Added lowBattery popup notification and cronie as dependence

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-pacman -S i3 compton conky nitrogen rofi maim xclip playerctl
+pacman -S i3 compton conky nitrogen rofi maim xclip playerctl cronie
 cp -R .i3 ~/.config/i3
 cp .compton.conf ~/
 cp .conkyrc ~/
+
+crontab ~/.config/i3/crontabWork.txt
+sudo systemctl start cronie.service
+sudo systemctl enable cronie.service
 
 git clone https://github.com/unix121/i3wm-themer
 cd i3wm-themer/scripts/
